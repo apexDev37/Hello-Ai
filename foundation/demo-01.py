d56aae05f149e39566b09f709b3893c2523ccdf2
apexdev <mwangi.em37@gmail.com>
2023-12-05 23:18:14 +0300

    feat: Enhance with output parser
    Make the following changes:
    - Revert logic to handle model depr.
    - Optimize prompt with LLM suggestions.
    - Add parser to parse response into Python native-type.

--- foundation/demo-01.py
+++ foundation/demo-01.py
@@ -6,63 +6,69 @@ written by:   Eugene M.
 
 date:         nov-2023
 
-usage:        simple demo of using a prompt template to prompt an LLM.
+usage:        simple demo of using a prompt templates and output parser.
 """
 
-import datetime
-import openai
+import os
 
-from os import environ as env
-from dotenv import find_dotenv, load_dotenv
+import openai
+from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 
+CHATGPT_MODEL: str = "gpt-3.5-turbo"
 
-CHATGPT_MODEL: str = 'gpt-3.5-turbo'
 
 def main() -> None:
-  """ Script entry-point func. """
+    """Script entry-point func."""
 
-  # Load your Open AI, API key
-  # If you don't have one, see:
-  # https://platform.openai.com/account/api-keys
-  ENV_FILE = find_dotenv()
-  if ENV_FILE:
-    load_dotenv()
+    # Load your Open AI, API key
+    # If you don't have one, see:
+    # https://platform.openai.com/account/api-keys
+    _ = load_dotenv(dotenv_path=".envs/keys.env")
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-  openai.api_key = env.get('OPENAI_API_KEY')
+    # Initialize model: Open AI's GPT LLM
+    chat = ChatOpenAI(
+        temperature=0.0,
+        model=CHATGPT_MODEL,
+    )
 
-  # Select LLM model
-  # handle deprecation of model
-  current_date = datetime.datetime.now().date()
-  target_date = datetime.date(2024, 6, 12)
+    # Ensure prompt consists of the following:
+    # desired action and precise context
+    greeting = "Jambo"
+    template = """
+      Analyze the text enclosed in backticks to identify whether it constitutes a typical and formal greeting.
+      If the text is indeed a greeting, provide information on the language it is in, three synonyms for the greeting, and specify five countries where it is commonly used.
 
-  if current_date > target_date:
-    llm_model = CHATGPT_MODEL
-  else:
-    llm_model = CHATGPT_MODEL + '-0301'
+      If the text is not a greeting, kindly suggest three universally recognized and formal greetings from around the world at random.
+      Text: `{customer_greeting}`
+      {format_instructions}
+    """
 
-  # Initialize model: Open AI's GPT LLM
-  chat = ChatOpenAI(temperature=0.0, model=llm_model,)
+    prompt_template = ChatPromptTemplate.from_template(template)
 
-  # Ensure prompt consists of the following:
-  # desired action and precise context
-  greeting = 'hello'
-  template = """
-    Determine if the provided text delimited in backticks is a common and
-    formal greeting. If it is a greeting, determine the language of the provided
-    greeting, three synonyms for the given text, and five countries that it is
-    most commonly used. Else, recommend three common and formal greetings used around
-    the world at random.
-    text: `{customer_greeting}`
-  """
+    # Leverage parser to parse the LLM response
+    # into a Python native type our script can consume.
+    greeting_schema = ResponseSchema(
+        name="greeting",
+        description="True, if the text is a typical and formal greeting, else False if not or unknown",
+    )
 
-  prompt_template = ChatPromptTemplate.from_template(template)
-  messages = prompt_template.format_messages(customer_greeting=greeting)
+    schemas = [greeting_schema]
+    output_parser = StructuredOutputParser.from_response_schemas(schemas)
+    format_instructions = output_parser.get_format_instructions()
 
-  response = chat(messages)
-  print(response.content)
+    messages = prompt_template.format_messages(
+        customer_greeting=greeting, format_instructions=format_instructions
+    )
+    response = chat(messages)
+
+    # Python native type our app can use.
+    data = output_parser.parse(response.content)
+    print(data)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
