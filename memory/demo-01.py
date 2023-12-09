9f2540f7aac05f94c49deba1d41078cd2711545c
apexdev <mwangi.em37@gmail.com>
2023-12-09 12:59:11 +0300

    refactor: Update demo script
    [Reason]
    Improve code readability/expression for demo.

--- memory/demo-01.py
+++ memory/demo-01.py
@@ -4,57 +4,27 @@ written by:   Eugene M.
 
 date:         dec-2023
 
-demo:         Conversation-Dialogs: dialog and query an LLM on previously discussed
-              user experiences.
+demo:         Reviving-Memories: query an LLM based on previous conversations.
 """
 
-import os
 
-import openai
-from dotenv import load_dotenv
 from langchain.chains import ConversationChain
-from langchain.chat_models import ChatOpenAI
-from langchain.chat_models.base import BaseChatModel
 from langchain.memory import ConversationBufferMemory
-
-from utils.constants import Commands
-
-CHATGPT_MODEL: str = "gpt-3.5-turbo"
-
-
-def setup_openai_model(model: str = CHATGPT_MODEL) -> BaseChatModel:
-    """
-    Load API key from env file and returns Open AI model.
-
-    Note:
-        Defaults to use `gpt-3.5-turbo`.
-    """
-
-    # If you don't have one, see:
-    # https://platform.openai.com/account/api-keys
-    _ = load_dotenv(dotenv_path=".envs/keys.env")
-    openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-    return ChatOpenAI(
-        temperature=0.0,
-        model=CHATGPT_MODEL,
-    )
-
-
-def init_interactive_conversation() -> None:
-    """Initiates an interactive user prompt loop."""
-
-    while True:
-        user_input = input("[prompt] >>> ")
-        if user_input.strip().lower() in Commands.TERMINATE:
-            break
-        yield user_input
+from utils.helpers import (
+    init_interactive_conversation,
+    read_conversation_from,
+    setup_for_query,
+    setup_gpt_model,
+    update_memory_context,
+)
 
 
 def main() -> None:
-    """Script entry-point func."""
+    """
+    Script entry-point func.
+    """
 
-    llm = setup_openai_model()
+    llm = setup_gpt_model(version="3.5", variant="turbo")
     memory = ConversationBufferMemory()
 
     # Create a chain to store history of conversations
@@ -64,6 +34,12 @@ def main() -> None:
         memory=memory,
         verbose=False,
     )
+
+    # Load past conversation and pass to the LLM's memory context.
+    conversations = read_conversation_from(file="memory/data/conversation.txt")
+    update_memory_context(memory, conversations)
+
+    setup_for_query(memory)
 
     # Launch an interactive conversation in your terminal.
     for user_prompt in init_interactive_conversation():
