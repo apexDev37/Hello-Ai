3b9d979156ff254987bc1674cdeadf129ca9dede
apexdev <mwangi.em37@gmail.com>
2023-12-09 12:56:45 +0300

    feat: Add utils module
    Add utils to reuse common demo functions.
    Abstract utils to de-clutter demo scripts and improve code readability.

---
+++ memory/utils/constants.py
@@ -0,0 +1,16 @@
+"""
+written by:   Eugene M.
+              https://github.com/apexDev37
+
+date:         dec-2023
+
+usage:        reusable constants for the `hello-ai` repo.
+"""
+
+
+class Commands:
+    """
+    Commands to manage the interactive user terminal prompt.
+    """
+
+    TERMINATE = {"quit", "end", "bye"}
